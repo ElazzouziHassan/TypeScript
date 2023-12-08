a144716b39368d297efe926f3cbe212c175bafae
wizardy <wizardhassan99@gmail.com>
2023-12-08 20:19:42 +0100

    commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let a:number = 8;
+let b:number = 20;
+
+const isAgreaterThanB = () =>{
+  if (a > b) {
+    return "Yes, A is greater than B"
+  } else{
+    return "No, A is not greater than B"
+  }
+}
+
+let result:string = isAgreaterThanB();
+
+console.log(result)
+
