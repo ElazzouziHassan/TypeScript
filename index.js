9e49f2a322156712272f73c143f7e32dd6da1d2d
wizardy <wizardhassan99@gmail.com>
2023-12-08 20:13:03 +0100

    commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-let message = "Lets start learing TypeScript !";
-console.log(message);
+var a = 8;
+var b = 20;
+var isAgreaterThanB = function () {
+    if (a > b) {
+        return "Yes, A is greater than B";
+    }
+    else {
+        return "No, A is not greater than B";
+    }
+};
+var result = isAgreaterThanB();
+console.log(result);
