7d8dadd5ba8aa56771ba0b8b2ff073d950d49a89
wizardy <wizardhassan99@gmail.com>
2023-12-15 18:00:02 +0100

    commit

--- public/1-data-types/index.js
+++ public/1-data-types/index.js
@@ -1,6 +1,16 @@
 "use strict";
-// Simple Types.
+// Simple Types :
 let bool = false;
 let num = 7;
 let fullName = 'Elazzouzi Hassan';
 let list = [1, 2, 3];
+// Special Types :
+// type any :
+let notSure = "I am not sure about the type yet !";
+notSure = false;
+// type unknown : unknown is a similar, but safer alternative to any.
+let unk = 7;
+unk = "Text"; // No error
+// unknown is best used when you don't know the type of data being typed. To add a type later, you'll need to cast it.
+// Casting is when we use the "as" keyword to say property or variable is of the casted type.
+unk = true;
--- src/1-data-types/index.ts
+++ src/1-data-types/index.ts
@@ -1,7 +1,23 @@
-// Simple Types.
+// Simple Types :
 let bool: boolean = false;
 let num: number = 7
 let fullName: string = 'Elazzouzi Hassan'
 let list: Array<number> = [1, 2, 3]
+
+// Special Types :
+
+  // type any :
+  let notSure: any = "I am not sure about the type yet !";
+  notSure = false;
+
+  // type unknown : unknown is a similar, but safer alternative to any.
+  let unk: unknown = 7;
+  unk = "Text" // No error
+
+  // unknown is best used when you don't know the type of data being typed. To add a type later, you'll need to cast it.
+  // Casting is when we use the "as" keyword to say property or variable is of the casted type.
+  unk = true as boolean;
+
+
 
 
