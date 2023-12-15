1b0ecc3cc0665987651b78a8fde925914b998d9d
wizardy <wizardhassan99@gmail.com>
2023-12-15 17:50:38 +0100

    commit

--- public/1-data-types/index.js
+++ public/1-data-types/index.js
@@ -1,1 +1,6 @@
 "use strict";
+// Basic Types.
+let bool = false;
+let num = 7;
+let fullName = 'Elazzouzi Hassan';
+let list = [1, 2, 3];
---
+++ src/1-data-types/index.ts
@@ -0,0 +1,5 @@
+// Basic Types.
+let bool: boolean = false;
+let num: number = 7
+let fullName: string = 'Elazzouzi Hassan'
+let list: Array<number> = [1, 2, 3]
