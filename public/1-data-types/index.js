3ecb42b09e848a8ec102a4731e3226f55558042c
wizardy <wizardhassan99@gmail.com>
2023-12-15 18:02:39 +0100

    commit

--- public/1-data-types/index.js
+++ public/1-data-types/index.js
@@ -14,3 +14,7 @@ unk = "Text"; // No error
 // unknown is best used when you don't know the type of data being typed. To add a type later, you'll need to cast it.
 // Casting is when we use the "as" keyword to say property or variable is of the casted type.
 unk = true;
+// Type never : never effectively throws an error whenever it is defined.
+// ! - let neverVar: never = true; 
+// Error: Type 'boolean' is not assignable to type 'never'.
+// never is rarely used, especially by itself, its primary use is in advanced generics.
--- src/1-data-types/index.ts
+++ src/1-data-types/index.ts
@@ -18,6 +18,11 @@ let list: Array<number> = [1, 2, 3]
   // Casting is when we use the "as" keyword to say property or variable is of the casted type.
   unk = true as boolean;
 
+// Type never : never effectively throws an error whenever it is defined.
+// ! - let neverVar: never = true; 
+// Error: Type 'boolean' is not assignable to type 'never'.
+// never is rarely used, especially by itself, its primary use is in advanced generics.
+
 
 
 
